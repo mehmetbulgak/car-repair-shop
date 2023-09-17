1b07ecd53f8b3cfa5c4b7eedd667eab50a86500b
Mehmet Bulğak <45659493+mehmetbulgak@users.noreply.github.com>
2023-09-18 01:12:23 +0300

    ...

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://car-repair-shop.onrender.com',
+    baseUrl: 'https://car-repair-shop-backend.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
